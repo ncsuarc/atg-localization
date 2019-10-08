259acc6828106081a5c7c07fd483b50e446ca66a
Steven Diniz <stevendiniz@Stevens-MacBook-Pro.local>
2019-10-08 19:48:43 -0400

    Added for loop to check if light_ray is above ground

--- localizer.py
+++ localizer.py
@@ -6,12 +6,20 @@ ureg = pint.UnitRegistry()
 
 
 STEP_SIZE = 1 * ureg.meter
-
+NUM_STEPS = 200
 
 @ureg.wraps((ureg.meter, ureg.meter), (ureg.meter, ureg.radian, ureg.radian))
-def find_ground(agl: float, east_angle: float, north_angle: float) -> Tuple[float, float]:
+def find_ground(agl: float, east_angle: float, north_angle: float, pos: float) -> Tuple[float, float]:
     v_east = STEP_SIZE * math.sin(east_angle)
     v_north = STEP_SIZE * math.sin(north_angle)
-    v_alt = -1 * math.cos(north_angle) * math.cos(east_angle)
+    v_alt = -STEP_SIZE * math.cos(north_angle) * math.cos(east_angle)
+    pos_north, pos_east, pos_alt = pos
 
-    # TODO: Do calculations
+    for i in range(NUM_STEPS):
+        if pos_alt <= 0:
+            return pos_north, pos_east
+        pos_north += v_north
+        pos_east += v_east
+        pos_alt += v_alt
+    return None
+        
