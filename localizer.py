aa734696d487d817776b287f2a43081b74fa7051
Eli W. Hunter <hunter.eli.w@gmail.com>
2019-10-16 20:40:00 -0400

    Format localizer.py

--- localizer.py
+++ localizer.py
@@ -1,5 +1,5 @@
 import pint
-from typing import Tuple
+from typing import Tuple, Optional
 import math
 
 ureg = pint.UnitRegistry()
@@ -8,18 +8,32 @@ ureg = pint.UnitRegistry()
 STEP_SIZE = 1 * ureg.meter
 NUM_STEPS = 200
 
-@ureg.wraps((ureg.meter, ureg.meter), (ureg.meter, ureg.radian, ureg.radian))
-def find_ground(agl: float, east_angle: float, north_angle: float, pos: float) -> Tuple[float, float]:
-    v_east = STEP_SIZE * math.sin(east_angle)
-    v_north = STEP_SIZE * math.sin(north_angle)
-    v_alt = -STEP_SIZE * math.cos(north_angle) * math.cos(east_angle)
-    pos_north, pos_east, pos_alt = pos
+
+@ureg.wraps(
+    (ureg.meter, ureg.meter),  # return
+    (ureg.meter, ureg.meter, ureg.meter, ureg.radian, ureg.radian),  # param
+)
+def find_ground(
+    pos_agl: float,
+    pos_north: float,
+    pos_east: float,
+    angle_north: float,
+    angle_east: float,
+) -> Optional[Tuple[float, float]]:
+    """
+    Args:
+        agl: AGL altitude
+        pos: North, East coordinates
+        angle: North, East radians
+    """
+    v_north = STEP_SIZE * math.sin(angle_north)
+    v_east = STEP_SIZE * math.sin(angle_east)
+    v_alt = -STEP_SIZE * math.cos(angle_north) * math.cos(angle_east)
 
     for i in range(NUM_STEPS):
-        if pos_alt <= 0:
+        if pos_agl <= 0:
             return pos_north, pos_east
         pos_north += v_north
         pos_east += v_east
-        pos_alt += v_alt
+        pos_agl += v_alt
     return None
-        
