fed14af7e2b1fa52c12c5dcdb559b37e7f0a0d23
Steven Diniz <stdiniz01@yahoo.com>
2019-10-23 19:58:01 -0400

    consistently use n,e,d coordinates

--- localizer.py
+++ localizer.py
@@ -6,9 +6,7 @@ NUM_STEPS = 200
 
 
 def find_ground(
-    pos_agl: float,
-    pos_north: float,
-    pos_east: float,
+    height: float,
     angle_north: float,
     angle_east: float,
 ) -> Optional[Tuple[float, float]]:
@@ -18,14 +16,18 @@ def find_ground(
         pos: North, East coordinates
         angle: North, East radians
     """
+    pos_north = 0
+    pos_east = 0
+    pos_down = 0
+
     v_north = math.sin(angle_north) * STEP_SIZE
     v_east = math.sin(angle_east) * STEP_SIZE
-    v_alt = math.cos(angle_north) * math.cos(angle_east) * -STEP_SIZE
+    v_down = math.cos(angle_north) * math.cos(angle_east) * STEP_SIZE
 
     for _ in range(NUM_STEPS):
-        if pos_agl <= 0:
+        if pos_down > height:
             return pos_north, pos_east
         pos_north += v_north
         pos_east += v_east
-        pos_agl += v_alt
+        pos_down += v_down
     return None
