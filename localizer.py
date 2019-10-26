b4db93d654799191eb041bc2bf9292d93c8b1981
Steven Diniz <stdiniz01@yahoo.com>
2019-10-26 15:31:38 -0400

    Add documentation

--- localizer.py
+++ localizer.py
@@ -2,30 +2,42 @@ from typing import Tuple, Optional
 import math
 
 STEP_SIZE = 1
+""" Assume we step 1 meter toward target until we hit the ground """
+
 NUM_STEPS = 200
+""" The maximum number of steps before test fails """
 
 
 def find_ground(
     height: float, angle_north: float, angle_east: float
 ) -> Optional[Tuple[float, float]]:
     """
+    Return the north and east distance from target to gimble.
+
+    Let height, north angle, and east angle be given by the caller, and assume the 
+    gimble starts at location <0, 0, 0> (<north, east, down>). We then image a 
+    light ray takes 1 meter steps from the gimble towards the target. As we step, 
+    we add the change in distance, relative to the respective plane, to the current 
+    position of the light ray. Once the downward position of the ray is greater 
+    or equal to the height of the gimble, we return the north and east distance 
+    from the gimble to the light ray. 
+
     Args:
-        agl: AGL altitude
-        pos: North, East coordinates
+        height: altitude of gimble
         angle: North, East radians
     """
-    pos_north = 0.0
-    pos_east = 0.0
-    pos_down = 0.0
+    pos_north = 0.0 # meters
+    pos_east = 0.0 # meters
+    pos_down = 0.0 # meters
 
-    v_north = math.sin(angle_north) * STEP_SIZE
-    v_east = math.sin(angle_east) * STEP_SIZE
-    v_down = math.cos(angle_north) * math.cos(angle_east) * STEP_SIZE
+    step_north = math.sin(angle_north) * STEP_SIZE # meters
+    step_east = math.sin(angle_east) * STEP_SIZE # meters
+    step_down = math.cos(angle_north) * math.cos(angle_east) * STEP_SIZE # meters
 
     for _ in range(NUM_STEPS):
         if pos_down > height:
             return pos_north, pos_east
-        pos_north += v_north
-        pos_east += v_east
-        pos_down += v_down
+        pos_north += step_north
+        pos_east += step_east
+        pos_down += step_down
     return None
--- test_localizer.py
+++ test_localizer.py
@@ -18,6 +18,32 @@ def test_simple():
 
 
 def test_complex():
+    """
+    Check that find_ground returns a coordinates within 0.5 meters of target
+    
+    We started with a rotation matrix formed by combining rotation about the North
+    and East axes. Then, we multiplied the matrix by the vector <0,0,1>, finding 
+    where the camera was pointing. We tested if this vector had the same direction 
+    as the offset vector from the gimbal to the ground. In other words, the vector 
+    we tested against was a unit vector pointed towards the ground. This forms a 
+    solveable system of equations for phi and theta, which can be used to generate 
+    test cases. We solved manually, but then we used WolframAlpha to solve the 
+    system of equations.
+
+    URL for math: solve for \theta and \phi \:\frac{x}{\sqrt{\left(x^2 + y^2 + 
+    z^2\right)}}=sin\left(\theta \right)cos\left(\phi \right),\:\frac{y}{\sqrt
+    {\left(x^2 + y^2 + z^2\right)}}=sin\left(\phi \right),\:\frac{z}{\sqrt{\left
+    (x^2 + y^2 + z^2\right)}}=cos\left(\theta \right)cos\left(\phi \right) 
+    - Wolfram|Alpha
+
+    Unit tests were generating by forming a (5,3) Numpy matrix and filling the 
+    first 3 rows with values between 0 and 150. The remaining two rows (phi 
+    and theta) were filled out with a list comprehension using the formula 
+    derived above.
+
+    We then took the values from the Numpy matrix and hardcoded them into
+    a few tests.
+    """
     n, e = find_ground(10, 0, math.asin(5 / math.sqrt(125)))
     assert math.isclose(0, n, rel_tol=TOLERANCE)
     assert math.isclose(5, e, rel_tol=TOLERANCE)
